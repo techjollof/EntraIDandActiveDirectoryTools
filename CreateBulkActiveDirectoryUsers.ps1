7e506ff4f9166bade20be6533986b9516b3c755b
TechJollof <pastorndt@gmail.com>
2023-12-29 13:29:58 +0800

    OU Validation

--- CreateBulkActiveDirectoryUsers.ps1
+++ CreateBulkActiveDirectoryUsers.ps1
@@ -1,16 +1,36 @@
 # Import active directory module for running AD cmdlets
-Import-Module ActiveDirectory
+# Import-Module ActiveDirectory
+# $Session = New-PSSession -ComputerName ITPROHUBDC
+# Invoke-Command -Session $Session {Import-Module ActiveDirectory}
+
+# for printing input header
+function HeaderPrint([string]$text) {
+    Write-host "`n################### $text ################# "
+}
+
+#Validate OU
+function OuValidation {
+    [string]$OUPathDN = $((Write-Host "`n>> Specify the Organization Unit (OU) distinguishedName. Eg. CN=Dani,CN=Users,DC=ITPH,DC=lab : "-ForegroundColor yellow -NoNewLine );Read-Host)
+
+    If([adsi]::Exists("LDAP://$OUPathDN") -eq $false){
+        $OUPathDN = $((Write-Host "`n>> Ivalid! Organization Unit (OU) exist. Try again : "-ForegroundColor red -NoNewLine );Read-Host)
+    }else{
+        return $OUPathDN
+    }
+}
 
 # Define UPN and OU
-$Domain = "itpro.work.gd"
-$OuPath = "CN=Dani,CN=Users,DC=ITPH,DC=lab" #$((Write-Host "`n>> Specify the Organization Unit (OU) : "-ForegroundColor yellow -NoNewLine );Read-Host)
+HeaderPrint -text "Define the domain and Organization Unit (OU)"
+$Domain = $((Write-Host "`n>> Specify the domain. Eg. itpro.work.gd : "-ForegroundColor yellow -NoNewLine );Read-Host)
+
+[string]$OuPath = 
 
 # Store the data from NewUsersFinal.csv in the $ADUsers variable
 $ADUsers = Import-Csv .\SampleUsersToBeCreated.csv ";"
 
 $addressFormatInfo =@"
 
-    Exampled user FirstName = Alex and LastName = Fox and domain = JesusLovesYou.com
+    Example user FirstName = Alex and LastName = Fox and domain = JesusLovesYou.com
 
     1 - alex.fox@JesusLovesYou.com
     2 - a.fox@JesusLovesYou.com
@@ -23,34 +43,36 @@ $addressFormatInfo =@"
 
 
 # Password for user creation
-$Password = $((Write-Host "`n>> Enter the password to create users : "-ForegroundColor yellow -NoNewLine );Read-Host)
+
+$AccPassword = $((Write-Host "`n>> Enter the password to create users : "-ForegroundColor yellow -NoNewLine );Read-Host)
 
 # Define the eamil address forrmat to use if address formate is worng or invalid
 Write-Host "`n############## Choose user address format ############`n $addressFormatInfo"
-$addressFormat = $((Write-Host "`n>> Chose UserPrincipalName format incase of invalid address : "-ForegroundColor yellow -NoNewLine );Read-Host)
+$UPNFormat = $((Write-Host "`n>> Chose UserPrincipalName format (Incase of incorrect addresses) : "-ForegroundColor yellow -NoNewLine );Read-Host)
 
 #Same address for UPN and Eamil
 $upnEqSMTP = $((Write-Host "`n>> Do you want the same address for UserPrincipalName (login) or Primary Email (SMTP) Address [Yes(Y)/No(N)? "-ForegroundColor yellow -NoNewLine );Read-Host)
-if ($upnEqSMTP -in "Yes,Y".Split(",")){
-    $PrimaryEmailAddress =
+if ($upnEqSMTP -in "Yes,Y".ToLower().Split(",")){
+    $PSMTPFormate = $UPNFormat
 }else {
     Write-Host "`n############## Choose user address format Primary Email(SMTP) Address ############`n $addressFormatInfo"
-    $addressFormat = $((Write-Host "`n>> Chose Primary Email(SMTP) Address format : "-ForegroundColor yellow -NoNewLine );Read-Host)
-
+    $PSMTPFormate = $((Write-Host "`n>> Chose Primary Email(SMTP) Address format : "-ForegroundColor yellow -NoNewLine );Read-Host)
 }
-function CrateAddress ($choice, $firstName, $lastName) {
+
+
+#address formate selection function
+function CrateAddress ($addressFormat, $firstName, $lastName) {
     switch ($addressFormat) {
-        1 { $UPNorSMTPAddress = "$($firstName+"."+$lastName)@$Domain"; break }
-        2 { $UPNorSMTPAddress = "$($firstName[0]+"."+$lastName)@$Domain"; break }
-        3 { $UPNorSMTPAddress = "$($firstName[0]+$lastName)@$Domain"; break }
-        4 { $UPNorSMTPAddress = "$($lastName+"."+$firstName)@$Domain"; break }
-        5 { $UPNorSMTPAddress = "$($lastName[0]+"."+$firstName)@$Domain"; break }
-        6 { $UPNorSMTPAddress = "$($lastName[0]+$firstName)@$Domain"; break }
+        1 { $addressFormat = "$($firstName+"."+$lastName)@$Domain"; break }
+        2 { $addressFormat = "$($firstName[0]+"."+$lastName)@$Domain"; break }
+        3 { $addressFormat = "$($firstName[0]+$lastName)@$Domain"; break }
+        4 { $addressFormat = "$($lastName+"."+$firstName)@$Domain"; break }
+        5 { $addressFormat = "$($lastName[0]+"."+$firstName)@$Domain"; break }
+        6 { $addressFormat = "$($lastName[0]+$firstName)@$Domain"; break }
     }
     
-    return $UPNorSMTPAddress
+    return $addressFormat
 }
-
 
 
 # Loop through each row containing user details in the CSV file
@@ -77,7 +99,7 @@ foreach ($User in $ADUsers) {
         DisplayName = "$($User.lastname +" "+ $User.firstname)"
         Enabled = $True
         ChangePasswordAtLogon = $True
-        AccountPassword = (ConvertTo-secureString $Password -AsPlainText -Force)
+        AccountPassword = (ConvertTo-secureString $AccPassword -AsPlainText -Force)
     }
 }
 
